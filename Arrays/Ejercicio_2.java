e3db3f73ff8cd2e29407be07f8b242dcbe6791cf
Jenner-penaloza <penalozanietojennerenrique>
2026-08-20 14:53:22 -0500

    Nuevoss

---
+++ Arrays/Ejercicio_2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ejercicio_2 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        int [] arr = new int [10];
+
+        for (int i = 0; i < arr.length; i++){
+           
+           //int val = arr.length ;
+           System.out.println(arr[i] = i+1);
+        }
+
+        for (int n: arr){
+            System.out.println("n:" + n);
+        }
+
+        // DE MAYOR A MENOR
+
+
+        for (int i = arr.length-1; i >= 0; i--){
+            System.out.println(arr[i]);
+        }
+
+        for (int n: arr){
+            System.out.println("n:" + n);
+        }
+
+        entrada.close();
+    }
+}
---
+++ Arrays/Ejercicio_3.java
@@ -0,0 +1,39 @@
+
+
+public class Ejercicio_3 {
+    public static void main(String[] args){
+
+        int size = 10;
+        int [] primos = new int[size];
+        int contador = 0;
+
+        int numero = 2;
+
+        
+        while (contador < size){
+
+            int divisor = 0;
+            for (int i = 1; i <= numero; i++){
+                
+
+                if (numero % i == 0){
+                    divisor ++;
+                    //System.out.println("N:" + numero + " " + i);
+                }
+            }
+
+            if (divisor == 2){
+                primos[contador] = numero;
+                contador ++;
+            }
+
+            numero ++;
+            
+        }
+
+        for (int n: primos){
+            System.out.println("N:" + n);
+        }
+        
+    }   
+}
